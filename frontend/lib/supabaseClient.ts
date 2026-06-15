8554bc970ed7cd13ceb7a55eeadbdab14225dc10
ArnavTanay <arnavofficial77@gmail.com>
2026-06-15 20:32:01 +0530

    l_try

--- frontend/lib/supabaseClient.ts
+++ frontend/lib/supabaseClient.ts
@@ -1,5 +1,7 @@
 import { createBrowserClient } from '@supabase/ssr'
 
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
+
 export const supabase = createBrowserClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
