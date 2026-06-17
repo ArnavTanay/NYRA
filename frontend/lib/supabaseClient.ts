662b4f015862a8a3eee2195d6447b9cbea15cf7a
ArnavTanay <arnavofficial77@gmail.com>
2026-06-17 12:05:26 +0530

    ssr change

--- frontend/lib/supabaseClient.ts
+++ frontend/lib/supabaseClient.ts
@@ -1,9 +1,9 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createClient } from '@supabase/supabase-js'
 
 console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL)
 console.log("SUPABASE KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? "exists" : "UNDEFINED")
 
-export const supabase = createBrowserClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 )
